0a5c03d2ad41b7b83b43c7fea8d9c048eb99a282
cavalcante-html <leandrocavalcante2504@gmail.com>
2025-04-03 12:45:19 -0300

    Teste

--- ProfessorGlender/Lista03/Atv4_DiasCorridos.java
+++ ProfessorGlender/Lista03/Atv4_DiasCorridos.java
@@ -2,29 +2,30 @@ package ProfessorGlender.Lista03;
 import java.util.Scanner;
 
 public class Atv4_DiasCorridos {
-
     public static void main(String[] args) {
         
-        Scanner scan = new Scanner(System.in); 
-        int bissexto, anos, diasFinal, anosDias, bissextoDias, segundosAno;
+        Scanner sc = new Scanner(System.in); 
+        int bissexto, dias1, dias2, mes1, mes2, ano1, ano2, diasFinal, anosDias, bissextoDias, anos = 0;
 
-        System.out.println("Digite uma quantidade de anos para o calculo dos segundos: ");
-        anos = scan.nextInt();
-
-        // Linhas de código para se considerar anos bissextos. 
-        anosDias = anos * 365;
+        System.out.println("\n==== Digite a primeira data para o calculo de dia decorridos: ===");
+        System.out.println("> Dia: ");
+        dias1 = sc.nextInt();
+        System.out.println("> Mês: ");
+        mes1 = sc.nextInt();
+        System.out.println("> Ano: ");
+        ano1 = sc.nextInt();
+        
+        /* 
         bissexto = anos % 3;
 
         for (bissextoDias = 0; bissexto != 0;) {
             bissextoDias += 1;
             bissexto %= anos / 3;
-        }
-
+        }        
+        
         diasFinal = anosDias + bissextoDias;
-        segundosAno = 60 * 60 * 24 * diasFinal;
-
-        System.out.println("Exitem " + diasFinal + " dias e " + segundosAno + " segundos em " + anos + " anos." );
-
-        scan.close();
+        
+        System.out.println("Exitem " + diasFinal + " dias e " + " segundos em " + anos + " anos." );
+        */
     }
 }
