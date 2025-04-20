a4144df78f2488ef9307575e8786e155b92433a2
cavalcante-html <leandrocavalcante2504@gmail.com>
2025-04-20 19:08:50 -0300

    Finalizando o codigo da Atividade 6

--- ProfessorGlender/Lista04/Atv6_CalculoComissao.java
+++ ProfessorGlender/Lista04/Atv6_CalculoComissao.java
@@ -27,8 +27,6 @@ public class Atv6_CalculoComissao {
             System.out.println("\n--------------------------------------------\n");
         }
 
-        System.out.println("\n=============================================\n");
-
         for (int j = 0; j < produtos.length; j++) {
             vendaItens[j] = precos[j] * unidades[j];
             // Teste valor total = System.out.println("> Total da venda de " + produtos[j] + " = " + vendaItens[j]);
@@ -36,22 +34,43 @@ public class Atv6_CalculoComissao {
 
         System.out.println("\n=============================================\n");
 
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.println("== Item " + (i+1) + " - " + produtos[i] + " ==");
+            System.out.printf("> Preço: R$%.2f", precos[i]);
+            System.out.printf("\n> Quantidade Vendida: %.0f", unidades[i]);
+            System.out.printf("\n> Venda total: R$%.2f", vendaItens[i]);
+            System.out.println("\n--------------------------------------------\n");
+        }
+
+        System.out.println("\n=============================================\n");
+
         for (int i = 0; i < vendaItens.length; i++) {
             vendaTotal += vendaItens[i];
-            System.out.println("> Valor total: " + vendaTotal);
         }
 
         comissaoPorCento = 5 / 100.00; 
         comissao = comissaoPorCento * vendaTotal;
         salarioFinal = salarioVendedor + comissao;
-        
-        System.out.println("> Salario do " + nomeVendedor + " = " + salarioFinal);
+
+        System.out.print("> Valor total: " + vendaTotal + "\n> Salario do vendedor " + nomeVendedor + ": ");
+        System.out.printf("R$%.2f \n> Valor da Comissão: R$%.2f \n> Salario final do vendedor: R$%.2f \n", salarioVendedor, comissao, salarioFinal);
 
         System.out.println("\n=============================================\n");
 
-        for (int i = 0; i < produtos.length; i++) {
-            System.out.println("> Item: " + (i+1) + " " + produtos[i] + "\n> Preço: " + precos[i] + "\n> Quantidade Vendida: " + unidades[i]);
+        int maisVendido = 0;
+
+        for (int i = 0; i < unidades.length; i++) {
+            if (unidades[i] > i) {
+                maisVendido = i;
+            }
         }
+
+        System.out.println("> Item Mais vendido: " + produtos[maisVendido] + "\n> Posição no Estoque: " + (maisVendido + 1));
+        System.out.printf("> Preço: R$%.2f \n", precos[maisVendido]);
+
+        System.out.println("\n=============================================\n");
+
+        sc.close();
 
     }
 }
