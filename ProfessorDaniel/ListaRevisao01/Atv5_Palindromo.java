dee830aee2ce9934b1aa17b5f8e11f6c4ec8a381
cavalcante-html <leandrocavalcante2504@gmail.com>
2025-05-01 10:17:27 -0300

    Update

--- ProfessorDaniel/ListaRevisao01/Atv5_Palindromo.java
+++ ProfessorDaniel/ListaRevisao01/Atv5_Palindromo.java
@@ -1,4 +1,5 @@
 package ProfessorDaniel.ListaRevisao01;
+import java.util.Scanner;
 
 public class Atv5_Palindromo {
     public static void main(String[] args) {
@@ -6,13 +7,19 @@ public class Atv5_Palindromo {
         Scanner sc = new Scanner(System.in);
         String palindromo, reverso = "";
         
-        System.out.println("> Insira sua palavra: ");
+        System.out.print("\n> Insira sua palavra: ");
         palindromo = sc.next();
 
-        for (int i = palindromo.length(); i >= 0; i--) {
+        for (int i = palindromo.length() - 1; i >= 0; i--) {
+            reverso += palindromo.charAt(i);            
+        }
 
-            reverso = reverso + palindromo.charAt(i);
-            
+        System.out.println("\n--------------------------------------\n");
+
+        if (reverso.equalsIgnoreCase(palindromo)) {
+            System.out.println("> " + palindromo + "\n> " + reverso + "\n=== SUA PALAVRA É UM PÁLINDROMO ===\n");
+        } else {
+            System.out.println("> " + palindromo + "\n> " + reverso + "\n=== SUA PALAVRA NÃO É UM PÁLINDROMO ===\n");
         }
 
     }
